b3dd7a8316dd41e4ae7e95ea0795c30e3bf618ee
zain-mustafa <zainmustafa@live.com>
2020-12-11 03:50:50 -0500

    Section 8 - WIP

--- burger-builder/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,23 @@ import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aux';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state 
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render () {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
